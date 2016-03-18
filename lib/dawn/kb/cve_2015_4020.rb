a1723e79338d14f3f43bb70d90a0650c4e0ad3d8
Franco Lazzarino <flazzarino@gmail.com>
2016-03-18 13:06:52 -0500

    Update cve_2015_4020.rb

--- lib/dawn/kb/cve_2015_4020.rb
+++ lib/dawn/kb/cve_2015_4020.rb
@@ -23,7 +23,7 @@ module Dawn
             :applies=>["rails", "sinatra", "padrino"],
             :kind=>Dawn::KnowledgeBase::GEM_CHECK,
             :message=>message,
-            :mitigation=>"Please upgrade redcarpet gem to version 3.2.3 or later.",
+            :mitigation=>"Please upgrade RubyGems to 2.0.17; 2.2.5; 2.4.8 or later.",
             :aux_links=>[""]
            })
 
